62194698d43c987c35c48ec7d20afd4b993efbdb
alejandroarce03 <54712283+alejandroarce03@users.noreply.github.com>
2020-03-15 12:36:58 -0500

    Add files via upload

--- ShiftControl/src/ui/Main.java
+++ ShiftControl/src/ui/Main.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.Scanner;
 
 import customExceptions.RequiredFieldsException;
@@ -8,7 +10,7 @@ import customExceptions.UserExistException;
 import model.Shift;
 import model.ShiftControler;
 
-public class Main {
+public class Main implements Serializable{
 //Attributes
 	private static  Scanner lector;
 	private static Main main;
@@ -26,70 +28,148 @@ public class Main {
 		system.alphabet();
 		int option=0;
 		do {
-			try {
-		System.out.println("MENU");
-		System.out.println("1. Register user");
-		System.out.println("2. Assign Shift");
-		System.out.println("3. Attend shift");
-		System.out.println("4. Exit");
-		option = Integer.parseInt(lector.nextLine());
+			
+				System.out.println(system.getDate().getActual());
+				System.out.println("MENU");
+				System.out.println("0. Exit");
+				System.out.println("1. Register user");
+				System.out.println("2. Assign Shift");
+				System.out.println("3. Attend shift");
+				System.out.println("4. Refresh time");
+				System.out.println("5. Create type of shift");
+				System.out.println("6. Report of shifts of the user");
+				System.out.println("7. Report of users ot the shift");
+				System.out.println("8. Generate names");
+				option = Integer.parseInt(lector.nextLine());
 		switch(option) {
 		case 1: 
-			System.out.print("Enter Complete name: ");
+			long totalSum = 0;
+			System.out.print("Enter name: ");
 			String name = lector.nextLine();
-			System.out.print("Enter type of docuemnt(IDENTIFICATION_CARD,IDENTY_CARD,PASSPORT,CIVIL_REGISTRATION,FOREIGNER_ID): ");
-			String type = lector.nextLine();
+			System.out.print("Enter lastname: ");
+			String lastname = lector.nextLine();
+			System.out.println("Enter type of docuemnt");
+			System.out.println("1.IDENTIFICATION_CARD");
+			System.out.println("2.IDENTY_CARD,PASSPORT");
+			System.out.println("3.CIVIL_REGISTRATION");
+			System.out.println("4.FOREIGNER_ID");
+			System.out.println("5.PASSPORT");
+			int type = Integer.parseInt(lector.nextLine());
 			System.out.print("Enter Id: ");
+			String id = lector.nextLine();
 			try {
-				system.typeDocument(type);
-				String id = lector.nextLine();
+				system.searchUser(id);
+				System.out.print("Enter phone number: ");
+				String phone = lector.nextLine();
+				System.out.print("Enter adress: ");
+				String adress = lector.nextLine();
+				long startTime = System.currentTimeMillis();
+
 				try {
-					system.searchUser(id);
-					System.out.print("Enter phone number: ");
-					String phone = lector.nextLine();
-					System.out.print("Enter adress: ");
-					String adress = lector.nextLine();
-					try {
-						system.addUser(name, id, type, phone, adress);
-						System.out.println("The user has been registred successfully");
-					} catch (RequiredFieldsException e) {
-				
-						System.out.println(e.getMessage());
-					}
-				} catch (UserExistException e1) {
-					System.out.println(e1.getMessage());
+					system.addUser(name,lastname,id, type, phone, adress);
+					System.out.println("The user has been registred successfully");
+					totalSum= (System.currentTimeMillis()-startTime);
+					System.out.println("running time: "+totalSum);
+				} catch (RequiredFieldsException | TypeOfDocumentImproperException | IOException e) {
 			
+					System.out.println(e.getMessage());
 				}
-				
-			} catch (TypeOfDocumentImproperException e2) {
-				System.out.println(e2.getMessage());
+			} catch (UserExistException e1) {
+				System.out.println(e1.getMessage());
+
 			}
 			
 			
 			
 		break;
 		case 2:
+			long totalSum2=0;
 			System.out.print("Enter the id: ");
 			String idSearch = lector.nextLine();
-			String m = system.checkAttendShift(idSearch);
-			if(!m.equalsIgnoreCase("")) {
-			System.out.println(m);
-			}else {
-				System.out.println(system.registerShifts(idSearch));
-			}
+			System.out.println("Enter type of shift: ");
+			String typeShift = lector.nextLine();
+			long startTime2=System.currentTimeMillis();
+			try {
+				System.out.println(system.registerShifts(idSearch,typeShift));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+			totalSum2=(System.currentTimeMillis()-startTime2);
+			System.out.println("running time: "+totalSum2);
 		break;
 		case 3:
-			String msg =system.attendShift();
-			if(msg.equalsIgnoreCase(".")) {
-				System.out.println("No turns to atend");
-			}else
-			System.out.println(msg);
-			
-		}
-			}catch(NumberFormatException n) {
-				System.out.println("Error choosing option, please use numbers");
+			long totalSum3=0;
+			long startTime3=System.currentTimeMillis();
+			int numShifts=system.attendShift();
+			if(numShifts==0) {
+				System.out.println("No turns to atend ");
+				totalSum3=(System.currentTimeMillis()-startTime3);
+				System.out.println("runnung time: "+totalSum3);
+			}else {
+				System.out.println(numShifts+" shifts has been attended");
+				for(int i=0;i<numShifts;i++) {
+					System.out.println("The shift number "+(i+1)+" the user was present?");
+					String s = lector.nextLine();
+					system.presentUser(s, i);
+				}
+				totalSum3=(System.currentTimeMillis()-startTime3);
+				System.out.println("runnung time: "+totalSum3);
 			}
+		break;
+		case 4:
+			long totalSum4=0;
+			long startTime4=System.currentTimeMillis();
+			system.time();
+			totalSum4=(System.currentTimeMillis()-startTime4);
+			System.out.println("runnung time: "+totalSum4);
+		break;
+		case 5:
+			long totalSum5=0;
+			System.out.print("Enter name of shift: ");
+			String nameShift = lector.nextLine();
+			System.out.println("Enter time in minutes. Example 1.5(1 minute and 30 seconds)");
+			long startTime5=System.currentTimeMillis();
+			double time = Double.parseDouble(lector.nextLine());
+			try {
+				system.createShiftType(nameShift, time);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			totalSum5=(System.currentTimeMillis()-startTime5);
+			System.out.println("runnung time: "+totalSum5);
 		
-	}while(option!=4);
+			
+		break;
+		case 6:
+			long totalSum6=0;
+			System.out.print("Enter id of user: ");
+			String idUser = lector.nextLine();
+			long startTime6=System.currentTimeMillis();
+			System.out.println(system.reportAllShifts(idUser));
+			totalSum6=(System.currentTimeMillis()-startTime6);
+			System.out.println("runnung time: "+totalSum6);
+		break;
+		case 7:
+			long totalSum7=0;
+			System.out.print("Enter num of shift: ");
+			String nShift = lector.nextLine();
+			long startTime7=System.currentTimeMillis();
+			System.out.println(system.reportShiftWithUsers(nShift));
+			totalSum7=(System.currentTimeMillis()-startTime7);
+			System.out.println("runnung time: "+totalSum7);
+		break;
+		case 8:
+			try {
+				system.generateNames();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+			
+		
+	}while(option!=0);
   }
 }
